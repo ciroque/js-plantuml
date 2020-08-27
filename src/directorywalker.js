5311a08f3a86c450d6ce4c54d522857548da5c1b
Steve Wagner <ciroque@live.com>
2020-08-27 12:25:21 -0700

    Renamed per conventions

--- src/directorywalker.js
+++ src/directorywalker.js
@@ -27,7 +27,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const DirectoryWalker = (opts) => {
+const Directorywalker = (opts) => {
     const options = { ...{ extensions: ['.js', '.jsx'], excludePaths: [], ...opts } };
     const isAcceptedFileType = (entry) => entry.isFile() && options.extensions.includes(path.extname(entry.name).toLowerCase());
     const isAcceptedDirectory = (entry) => entry.isDirectory() && !options.excludePaths.includes(entry.name);
@@ -44,4 +44,4 @@ const DirectoryWalker = (opts) => {
     };
 };
 
-export default DirectoryWalker;
+export default Directorywalker;
